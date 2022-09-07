1abc891d0fe63d49015cf929a98367bf7f2fed0c
bduff9 <bduff9@gmail.com>
2022-09-06 20:13:33 -0500

    Added tailwind and web3 features

---
+++ components/index.ts
@@ -0,0 +1,2 @@
+export { Web3Address } from './Web3Address'
+export { Web3Button } from './Web3Button'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
+
+import { Web3ContextProvider } from '../context'
+
+import 'react-toastify/dist/ReactToastify.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ContextProvider>
+      <>
+        <Component {...pageProps} />
+        <ToastContainer
+          hideProgressBar
+          position="bottom-right"
+          autoClose={2000}
+        />
+      </>
+    </Web3ContextProvider>
+  )
 }
 
 export default MyApp
